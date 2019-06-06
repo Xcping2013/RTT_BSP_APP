7c630b04c93fd84d354a60042ffbed9fb30198de
canping xie <canping.xie@incubecn.com>
2019-06-06 16:32:42 +0800

    增加AD7124驱动，官方函数标识位有bug，已修改

---
+++ cubeCode/cube_example.c
@@ -0,0 +1,20 @@
+
+#include "cube_example.h"
+
+///*ADC_AnalogWatchdog
+
+//#define RANGE_12BITS                   ((uint32_t) 4095)    /* Max value with a full range of 12 bits */
+//#define USERBUTTON_CLICK_COUNT_MAX     ((uint32_t)    4)    /* Maximum value of variable "UserButtonClickCount" */
+
+//#define ADCCONVERTEDVALUES_BUFFER_SIZE ((uint32_t) 256)     /* Size of array containing ADC converted values */
+
+//#define TIMER_FREQUENCY                ((uint32_t) 1000)    /* Timer frequency (unit: Hz). With a timer 16 bits and time base freq min 1Hz, range is min=1Hz, max=32kHz. */
+//#define TIMER_FREQUENCY_RANGE_MIN      ((uint32_t)    1)    /* Timer minimum frequency (unit: Hz). With a timer 16 bits, maximum frequency will be 32000 times this value. */
+//#define TIMER_PRESCALER_MAX_VALUE      (0xFFFF-1)           /* Timer prescaler maximum value (0xFFFF for a timer 16 bits) */
+
+//__IO uint16_t   aADCxConvertedValues[ADCCONVERTEDVALUES_BUFFER_SIZE];
+//uint8_t         ubAnalogWatchdogStatus = RESET;  /* Set into analog watchdog interrupt callback */
+//uint8_t         ubUserButtonClickCount = 0;      /* Count number of clicks: Incremented after User Button interrupt */
+//__IO uint8_t    ubUserButtonClickEvent = RESET;  /* Event detection: Set after User Button interrupt */
+
+//*/
